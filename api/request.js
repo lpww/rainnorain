507c39d555fcf0b40e578c76e6d85d6940985aa6
Stuart Wilson <stuart.wilson@graze.com>
2014-11-17 13:39:45 +0000

    Added some comments to explain code more

--- api/request.js
+++ api/request.js
@@ -1,17 +1,31 @@
+// Import our hidden endpoint and the Node HTTP module
 var endpoint = require('./endpoint'),
     http = require('http');
 
 module.exports = {
+    // Our single method for retrieving data
     get: function getResponse(callback) {
+
+        // Make a get request on our endpoint
+        // Returns an IncomingMessage object (res)
         http.get(endpoint, function httpGetRequest(res){
+            // This stores our returned data
             var data = '';
 
+            // This ensures our data is formatted correctly
             res.setEncoding('utf8');
 
+            // The data event can be called multiple times with different
+            // chunks of the requested data
+            // (because of the asynchronous nature of the function)
+
+            // Here we append it to the data var to make sure we return
+            // a complete JSON object
             res.on('data', function onBodyResponse(chunk) {
                 data += chunk;
             });
 
+            // Fire our callback once the request is fully returned
             res.on('end', function onBodyResponse() {
                 callback(data);
             });
--- app.js
+++ app.js
@@ -3,11 +3,15 @@ var connect = require('connect'),
     api = require('./api/request'),
     app = connect();
 
+// Routing for all static assets such as index.html
 app.use(serveStatic(__dirname)).listen(3000);
 
+// Specific routing for /api
 app.use('/api', function apiResponse(req, res) {
+    // Return the correct header
     res.setHeader('Content-Type', 'application/json');
 
+    // Call api.get() to return a response when /api is visited
     api.get(function responseCallback(data) {
         res.end(data);
     });
