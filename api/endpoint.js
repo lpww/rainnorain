084f49153c75605e66659aadbbb716363d36708a
lpww <t.treffry@gmail.com>
2014-12-04 10:15:37 -0800

    security updates

--- api/endpoint.js
+++
@@ -1,7 +0,0 @@
-// Secret API endpoint
-// Fill this out and save as endpoint.js
-// but do not commit these secret deets
-module.exports = {
-	"forecast": "http://api.aerisapi.com/forecasts/:auto?filter=6hr&plimit=3&from=today+T06:00:00-08:00&client_id=iHeZxH8vaFLuunuhMacVq&client_secret=gsqSJT5AwiZ9UD6LFE30WXXJZE5udXOTD0m57o9J",
-	"gps": "http://api.aerisapi.com/places/closest?p=" + "100" + "," + "100" + "&client_id=iHeZxH8vaFLuunuhMacVq&client_secret=gsqSJT5AwiZ9UD6LFE30WXXJZE5udXOTD0m57o9J",
-}
